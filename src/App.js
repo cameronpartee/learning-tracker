40d65313a3311ab11857abc29683b9503498f6f8
Cameron Partee <cp3082@nyu.edu>
2021-09-13 11:17:00 -0400

    inital project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Task from "./components/Task";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Task />
+    </div>
+  );
+}
+
+export default App;
