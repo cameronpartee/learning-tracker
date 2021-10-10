345207fc5e4b7b5d45bc64b7ccca0300d4bdea7e
Cameron Partee <cp3082@nyu.edu>
2021-10-10 10:51:35 -0400

    remove dummy data,replace with firebase fetch call

--- src/components/Task/TaskList.js
+++ src/components/Task/TaskList.js
@@ -1,70 +1,73 @@
 import Task from "./Task";
-
-const DUMMY_TASKS = [
-  {
-    topic: "npm vs npx",
-    description: "When should you use npm vs npx?",
-    difficulty: "Easy",
-    date: "9/13/21",
-    timeEstimate: "30mins",
-    resources:
-      "https://stackoverflow.com/questions/50605219/difference-between-npx-and-npm",
-  },
-  {
-    topic: "props.children",
-    description: "Why should you use props.children?",
-    difficulty: "Medium",
-    date: "9/13/21",
-    timeEstimate: "40mins",
-    resources:
-      "https://stackoverflow.com/questions/49706823/what-is-this-props-children-and-when-you-should-use-it",
-  },
-  {
-    topic: "context api",
-    description: "What is the React context API?",
-    difficulty: "Medium",
-    date: "9/13/21",
-    timeEstimate: "45mins",
-    resources: "https://dev.to/aryclenio/how-to-use-the-react-context-api-4dle",
-  },
-  {
-    topic: "flexbox",
-    description: "What are all the flexbox styling rules?",
-    difficulty: "Easy",
-    date: "9/13/21",
-    timeEstimate: "15mins",
-    resources: "https://css-tricks.com/snippets/css/a-guide-to-flexbox/",
-  },
-  {
-    topic: "multiple classes",
-    description: "How do you include multiple classes?",
-    difficulty: "Easy",
-    date: "9/13/21",
-    timeEstimate: "5mins",
-    resources:
-      "https://github.com/cammyp/design-challenge/blob/main/src/components/Layout/HeaderCartButton.js",
-  },
-  {
-    topic: "react router",
-    description: "What is react router used for?",
-    difficulty: "Easy",
-    date: "9/15/21",
-    timeEstimate: "2mins",
-    resources:
-      "https://www.geeksforgeeks.org/reactjs-router/#:~:text=React%20Router%20is%20a%20standard,in%20sync%20with%20the%20URL.com",
-  },
-];
+import { useEffect, useState } from "react";
+import classes from "./TaskList.module.css";
 
 const TaskList = () => {
-  const taskList = DUMMY_TASKS.map((task) => (
+  const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch(
+        "https://learningtracker-a8476-default-rtdb.firebaseio.com/tasks.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseData = await response.json();
+      const loadedTasks = [];
+
+      for (const key in responseData) {
+        loadedTasks.push({
+          id: key,
+          topic: responseData[key].topic,
+          description: responseData[key].description,
+          difficulty: responseData[key].difficulty,
+          date: responseData[key].date,
+          timeEstimate: responseData[key].timeEstimate,
+          url: responseData[key].resources,
+        });
+      }
+
+      setTasks(loadedTasks);
+      setIsLoading(false);
+    };
+
+    fetchTasks().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.TasksLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.TasksError}>
+        <p>{httpError}!</p>
+      </section>
+    );
+  }
+
+  const taskList = tasks.map((task) => (
     <Task
+      key={task.id}
       topic={task.topic}
-      des={task.description}
-      diff={task.difficulty}
+      description={task.description}
+      difficulty={task.difficulty}
       date={task.date}
-      time={task.timeEstimate}
-      url={task.resources}
-      res={"Read article"}
+      timeEstimate={task.timeEstimate}
+      url={task.url}
+      resources={"Read article"}
     />
   ));
 
