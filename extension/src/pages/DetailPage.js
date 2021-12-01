1d8124ff8a9a1ef6715c89474d1de4e8e91fcacd
cameronpartee <cameronepartee@gmail.com>
2021-11-30 23:04:49 -0500

    add iframe codesandbox to details page

--- extension/src/pages/DetailPage.js
+++ extension/src/pages/DetailPage.js
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { BsBackspaceFill, BsBackspaceReverseFill } from "react-icons/bs";
 import { useState, useEffect } from "react";
+import IFrameComp from "./iframe";
 
 const DetailView = (props) => {
   const [index, setIndex] = useState(0);
@@ -41,13 +42,13 @@ const DetailView = (props) => {
         </div>
         <div>Problem Statement</div>
         <div>{props.tasks !== undefined && props.tasks[index].description}</div>
-        <div>Code Sandbox:</div>
       </Text>
-      <Sandbox>
+      <IFrameComp />
+      {/* <Sandbox>
         <SandboxText>
           {props.tasks !== undefined && props.tasks[index].codeSnip}
         </SandboxText>
-      </Sandbox>
+      </Sandbox> */}
       <img src={props.tasks !== undefined && props.tasks[index].img} alt="" />
       <a
         href={props.tasks !== undefined && props.tasks[index].url}
@@ -67,27 +68,32 @@ const Container = styled.div`
   margin-left: 30px;
   display: flex;
   flex-direction: column;
+  overflow: hidden;
+  overflow-y: scroll;
 
   img {
     margin-top: 20px;
-    width: 90%;
+    width: 95%;
   }
 
   a {
     text-align: right;
-    margin-top: 20px;
-    margin-right: 50px;
+    margin-top: 10px;
+    margin-right: 35px;
     text-decoration: none;
     cursor: pointer;
+    margin-bottom: 35px;
+    font-size: 0.9em;
+    font-weight: 600;
   }
 `;
 
 const Text = styled.div`
-   svg {
+  svg {
     color: grey;
-   }
+  }
 
-   > div:nth-child(1) {
+  > div:nth-child(1) {
     display: flex;
     position: relative;
 
@@ -101,7 +107,7 @@ const Text = styled.div`
       position: absolute;
       right: 50px;
     }
-  
+
     span {
       margin-left: 8px;
       margin-right: 8px;
@@ -110,7 +116,7 @@ const Text = styled.div`
   }
 
   > div:nth-child(2) {
-    margin-top: 45px;   
+    margin-top: 45px;
     margin-bottom: 15px;
     font-weight: 600;
     text-align: center;
@@ -119,20 +125,13 @@ const Text = styled.div`
   }
 
   > div:nth-child(3) {
-    margin-bottom: 40px;
+    margin-bottom: 25px;
     color: rgb(85, 85, 85);
     font-size: 1.5em;
     font-weight: 600;
     letter-spacing: 1px;
     text-align: center;
     width: 450px;
-  }
-
-  > div:last-child {
-    font-weight: 600;
-    color: rgb(10, 10, 10);
-    font-size 0.9em;
-    margin-bottom: 5px;
   }
 `;
 
