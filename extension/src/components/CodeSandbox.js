01530645eaca7429d9742cb3cc585fb6ba8c5644
cameronpartee <cameronepartee@gmail.com>
2021-12-12 23:33:07 -0500

    edit details pages styling

--- extension/src/components/CodeSandbox.js
+++ extension/src/components/CodeSandbox.js
@@ -2,7 +2,7 @@ const CodeSandbox = ({ src, title }) => {
   return (
     <iframe
       src={src}
-      style={{ width: "450px", height: "300px" }}
+      style={{ width: "450px", height: "150px" }}
       title={title}
       allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
       sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
