058a1e029c0570073025264f1be155add7209a1c
cameronpartee <cameronepartee@gmail.com>
2022-05-30 23:14:58 -0400

    work on adding pivot funcrtionality

--- v2/extension/src/App.js
+++ v2/extension/src/App.js
@@ -1,6 +1,17 @@
 import { TextField } from "@fluentui/react/lib/TextField";
 import { DefaultButton } from "@fluentui/react/lib/Button";
 import { useState } from "react";
+import {
+  IStyleSet,
+  Label,
+  ILabelStyles,
+  Pivot,
+  PivotItem,
+} from "@fluentui/react";
+
+const labelStyles = {
+  root: { marginTop: 10 },
+};
 
 // WRITE TO FIREBASE
 // STORE DATA
@@ -12,16 +23,46 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <div style={{ display: "grid", gridTemplateColumns: "65% auto" }}>
-        <div style={{ margin: "0 10px" }}>
-          <TextField
-            placeholder="Enter a website URL"
-            onChange={onClickHandler}
-          />
-        </div>
-        <div style={{ margin: "0 0px" }}>
-          <DefaultButton text="Submit" />
+    <div
+      className="App"
+      style={{
+        height: "100vh",
+      }}
+    >
+      <div>Enter URL</div>
+      <div
+        style={{
+          display: "flex",
+          justifyCntent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "80% auto",
+            width: "100%",
+          }}
+        >
+          <div
+            style={{
+              margin: "0 15px",
+            }}
+          >
+            <TextField
+              placeholder="Enter a website URL"
+              onChange={onClickHandler}
+            />
+          </div>
+          <div
+            style={{
+              margin: "0 0px",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            <DefaultButton text="Submit" />
+          </div>
         </div>
       </div>
     </div>
